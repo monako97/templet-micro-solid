4a8345aa72fd6238175f7fea6ba815b5394f6507
moneko <poi.nyaa@qq.com>
2023-08-25 00:34:31 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import '@/global.less';
 
 // 如果不需要在生命周期中操作, 可以将其删除然后重新运行
 // 微应用被挂载
-// export async function mount(props: Record<string, unknown>) {
-//   console.log("mount", props);
-// }
-// // 微应用被卸载
-// export async function unmount(props: Record<string, unknown>) {
-//   console.log("unmount", props);
-// }
+export async function mount(props: Record<string, unknown>) {
+  console.log("mount", props);
+}
+// 微应用被卸载
+export async function unmount(props: Record<string, unknown>) {
+  console.log("unmount", props);
+}
